73283917f52183f42c88901c88abc9afa6f8de85
Mohmmad Saleh Tehrani <saleh.teh2003@gmail.com>
2025-01-30 18:25:16 +0330

    create a input windows and more

--- Circuit1.py
+++ Circuit1.py
@@ -2,18 +2,16 @@ import schemdraw
 import schemdraw.elements as elm
 
 
-
-
-def draw_circuit1_PNP_off(d):
+def draw_circuit1_PNP_off(d,RB="RB",VCC="VCC",RC="RC",VBB="VBB"):
     d+= elm.Dot().at((-1.5033333333333332,1.8410523547181874e-16))
-    d += elm.Resistor().left().label('Rb 200kΩ')
-    d += elm.SourceV().down().label('VBB 5V')
+    d += elm.Resistor().left().label(RB)
+    d += elm.BatteryCell().down().label(VBB).reverse()
     d += elm.Line().length(7).right()
     d += elm.Line().length(0.5).up()
-    d += elm.SourceV().up().reverse().label('Vcc 10V')
+    d += elm.BatteryCell().up().label(VCC)
     d += elm.Dot().at((-0.7516666666666665,0.6966666666666668))
     d += elm.Line().length(0.5).up()
-    d += elm.Resistor().right().label('3kΩ')
+    d += elm.Resistor().right().label(RC)
     d += elm.Line().length(0.25).right()
     d += elm.Line().length(1.7).down()
     d += elm.Dot().at((-0.7516666666666667,-0.6966666666666665))
@@ -21,81 +19,73 @@ def draw_circuit1_PNP_off(d):
     d += elm.Ground()
     return d
     
-def draw_circuit1_PNP_Active(d):
-    d+= elm.Dot().at((-1.5033333333333332,1.8410523547181874e-16))
-    d += elm.BatteryCell().left().reverse().label('Vbe')
-    d += elm.Resistor().left().label('Rb 200kΩ')
-    d += elm.SourceV().down().label('VBB 5V')
-    d += elm.Line().length(10).right()
+def draw_circuit1_PNP_Active(d,RB="RB",VCC="VCC",RC="RC",VBB="VBB"):
+    d += elm.BatteryCell().left().label('VEB')
+    d += elm.Resistor().left().label(RB)
+    d += elm.BatteryCell().down().label(VBB).reverse()
+    d += elm.Line().length(9).right()
     d += elm.Line().length(0.5).up()
-    d += elm.SourceV().up().reverse().label('Vcc 10V')
-    d += elm.Dot().at((-0.7516666666666665,0.6966666666666668))
-    d += elm.SourceControlledI().label("ib").reverse()
-    d += elm.Resistor().right().label('3kΩ')
-    d += elm.Line().length(0.25).right()
-    d += elm.Line().length(1.7).down()
-    d += elm.Dot().at((-0.7516666666666667,-0.6966666666666665))
-    d += elm.Line().length(2.5).down()
-    d += elm.Ground()
-    return d
-
-def draw_circuit1_PNP_Sat(d):
-    d+= elm.Dot().at((-1.5033333333333332,1.8410523547181874e-16))
-    d += elm.BatteryCell().left().reverse().label('Vbe')
-    d += elm.Resistor().left().label('Rb 200kΩ')
-    d += elm.SourceV().down().label('VBB 5V')
-    d += elm.Line().length(10).right()
+    d += elm.BatteryCell().up().label(VCC)
     d += elm.Line().length(2).up()
-    d += elm.SourceV().up().reverse().label('Vcc 10V')
-    d += elm.Dot().at((-0.7516666666666665,0.6966666666666668))
-    d += elm.BatteryCell().label("vce").reverse()
-    d += elm.Resistor().right().label('3kΩ')
-    d += elm.Line().length(0.25).right()
-    d += elm.Line().length(1.7).down()
-    d += elm.Dot().at((-0.7516666666666667,-0.6966666666666665))
-    d += elm.Line().length(2.5).down()
+    d += elm.Resistor().left().label(RC)
+    d += elm.SourceControlledI().label("ib").down().reverse()
+    d += elm.Line().length(3).down()
+    d += elm.Ground()
+    return d
+
+def draw_circuit1_PNP_Sat(d,RB="RB",VCC="VCC",RC="RC",VBB="VBB"):
+    d += elm.BatteryCell().left().label('VEB')
+    d += elm.Resistor().left().label(RB)
+    d += elm.BatteryCell().down().label(VBB).reverse()
+    d += elm.Line().length(9).right()
+    d += elm.Line().length(0.5).up()
+    d += elm.BatteryCell().up().label(VCC)
+    d += elm.Line().length(2).up()
+    d += elm.Resistor().left().label(RC)
+    d += elm.BatteryCell().label("VEC").down().reverse()
+    d += elm.Line().length(3).down()
     d += elm.Ground()
     return d
     
 
-def draw_circuit1_NPN(d):
-    d += elm.SourceV().up().label('VBB 5V')
-    d += elm.Resistor().right().label('Rb 200kΩ')
+def draw_circuit1_NPN(d,RB="RB",VCC="VCC",RC="RC",VBB="VBB"):
+    d += elm.BatteryCell().up().label(VBB).reverse()
+    d += elm.Resistor().right().label(RB)
     transistor = d.add( elm.BjtNpn(circle=True).label('Q1').right() )
     d += elm.Line().length(1).up()
     d += elm.Line().length(0.5).right()
-    d += elm.Resistor().right().label('3kΩ')
-    d += elm.SourceV().down().reverse().label('Vcc 10V')
+    d += elm.Resistor().right().label(RC)
+    d += elm.BatteryCell().down().label(VCC)
     d += elm.Line().length(1.7).down()
     d += elm.Line().length(7.25).left()
     d += elm.Line().length(2.8).down().at(transistor.emitter)
     d += elm.Ground()
     return d
 
-def draw_circuit1_PNP(d):
+def draw_circuit1_PNP(d,RB="RB",VCC="VCC",RC="RC",VBB="VBB"):
     transistor = d.add( elm.BjtPnp2(circle=True).label('Q1').reverse())
-    d += elm.Resistor().left().label('Rb 200kΩ').at(transistor.base)
-    d += elm.SourceV().down().label('VBB 5V')
+    d += elm.Resistor().left().label(RB).at(transistor.base)
+    d += elm.BatteryCell().down().label(VBB).reverse()
     d += elm.Line().length(7).right()
     d += elm.Line().length(0.5).up()
-    d += elm.SourceV().up().reverse().label('Vcc 10V')
-    d += elm.Resistor().right().label('3kΩ').at(transistor.collector)
+    d += elm.BatteryCell().up().label(VCC)
+    d += elm.Resistor().right().label(RC).at(transistor.collector)
     d += elm.Line().length(0.21).right()
     d += elm.Line().length(1.7).down()
     d += elm.Line().length(2).down().at(transistor.emitter)
     d += elm.Ground()
     return d
 
-def draw_circuit1_NPN_off(d):
+def draw_circuit1_NPN_off(d,RB="RB",VCC="VCC",RC="RC",VBB="VBB"):
     d+= elm.Dot().at((-1.5033333333333332,1.8410523547181874e-16))
-    d += elm.Resistor().left().label('Rb 200kΩ')
-    d += elm.SourceV().down().reverse().label('VBB 5V')
+    d += elm.Resistor().left().label(RB)
+    d += elm.BatteryCell().down().label(VBB)
     d += elm.Line().length(7).right()
     d += elm.Line().length(0.5).up()
-    d += elm.SourceV().up().label('Vcc 10V')
+    d += elm.BatteryCell().up().label(VCC).reverse()
     d += elm.Dot().at((-0.7516666666666665,0.6966666666666668))
     d += elm.Line().length(0.5).up()
-    d += elm.Resistor().right().label('3kΩ')
+    d += elm.Resistor().right().label(RC)
     d += elm.Line().length(0.25).right()
     d += elm.Line().length(1.7).down()
     d += elm.Dot().at((-0.7516666666666667,-0.6966666666666665))
@@ -103,68 +93,68 @@ def draw_circuit1_NPN_off(d):
     d += elm.Ground()
     return d
     
-def draw_circuit1_NPN_Active(d):
-    d+= elm.Dot().at((-1.5033333333333332,1.8410523547181874e-16))
+def draw_circuit1_NPN_Active(d,RB="RB",VCC="VCC",RC="RC",VBB="VBB"):
+   
     d += elm.BatteryCell().left().reverse().label('Vbe')
-    d += elm.Resistor().left().label('Rb 200kΩ')
-    d += elm.SourceV().down().reverse().label('VBB 5V')
-    d += elm.Line().length(10).right()
+    d += elm.Resistor().left().label(RB)
+    d += elm.BatteryCell().down().label(VBB)
+    d += elm.Line().length(9).right()
     d += elm.Line().length(0.5).up()
-    d += elm.SourceV().up().label('Vcc 10V')
-    d += elm.Dot().at((-0.7516666666666665,0.6966666666666668))
-    d += elm.SourceControlledI().label("ib").reverse()
-    d += elm.Resistor().right().label('3kΩ')
-    d += elm.Line().length(0.25).right()
-    d += elm.Line().length(1.7).down()
-    d += elm.Dot().at((-0.7516666666666667,-0.6966666666666665))
-    d += elm.Line().length(2.5).down()
-    d += elm.Ground()
-    return d
-
-def draw_circuit1_NPN_Sat(d):
-    d+= elm.Dot().at((-1.5033333333333332,1.8410523547181874e-16))
-    d += elm.BatteryCell().left().reverse().label('Vbe')
-    d += elm.Resistor().left().label('Rb 200kΩ')
-    d += elm.SourceV().down().reverse().label('VBB 5V')
-    d += elm.Line().length(10).right()
+    d += elm.BatteryCell().up().label(VCC).reverse()
     d += elm.Line().length(2).up()
-    d += elm.SourceV().up().label('Vcc 10V')
-    d += elm.Dot().at((-0.7516666666666665,0.6966666666666668))
-    d += elm.BatteryCell().label("vce").reverse()
-    d += elm.Resistor().right().label('3kΩ')
-    d += elm.Line().length(0.25).right()
-    d += elm.Line().length(1.7).down()
-    d += elm.Dot().at((-0.7516666666666667,-0.6966666666666665))
-    d += elm.Line().length(2.5).down()
+    d += elm.Resistor().left().label(RC)
+    d += elm.SourceControlledI().label("ib").down()
+    d += elm.Line().length(3).down()
     d += elm.Ground()
     return d
 
+def draw_circuit1_NPN_Sat(d,RB="RB",VCC="VCC",RC="RC",VBB="VBB"):
+    d += elm.BatteryCell().left().reverse().label('Vbe')
+    d += elm.Resistor().left().label(RB)
+    d += elm.BatteryCell().down().label(VBB)
+    d += elm.Line().length(9).right()
+    d += elm.Line().length(0.5).up()
+    d += elm.BatteryCell().up().label(VCC).reverse()
+    d += elm.Line().length(2).up()
+    d += elm.Resistor().left().label(RC)
+    d += elm.BatteryCell().label("VCE").down()
+    d += elm.Line().length(3).down()
+    d += elm.Ground()
+    return d
+
+  
 def Analysis_for_circuit1_NPN(VBB,RB,Beta,VCC,RC):
     Ibase = (VBB-0.7)/RB
     if Ibase <= 0 :
-        return("off")
+        return((("off",0,0,VCC),draw_circuit1_NPN_off,"KVL 1: -VBB + (RB)*IB + VBE(ACTIVE) = 0"))
     Icollector = Beta * Ibase
+
+
     VCE = VCC - Icollector* RC
     if VCE > 0.2 :
-        return(("Active",Ibase,Icollector,VCE))
+        TEMP = "KVL 1: -VBB + (RB)*IB + VBE(ACTIVE) = 0 \n KVL 2: -VCC + RC * IC + VCE = 0 \n IC = BETA * IB"
+        return((("Active",Ibase,Icollector,VCE),draw_circuit1_NPN_Active,TEMP))
     else:
         Ibase = (VBB-0.8)/RB
         Icollector = (VCC - 0.2) / RC
         VCE = 0.2
-        return(("Sat",Ibase,Icollector,VCE))
+        TEMP = "KVL 1: -VBB + (RB)*IB + VBE(sat) = 0 \n IE = IC + IB"
+        return((("Sat",Ibase,Icollector,VCE),draw_circuit1_NPN_Sat,TEMP))
 
 def Analysis_for_circuit1_PNP(VBB,RB,Beta,VCC,RC):
     Ibase = (VBB-0.7)/RB
     if Ibase <= 0 :
-        return("off")
+        return((("off",0,0,VCC),draw_circuit1_PNP_off,"KVL 1: -VBB + (RB)*IB + VBE(ACTIVE) = 0"))
     Icollector = Beta * Ibase
     VEC = VCC - Icollector* RC
     if VEC > 0.2 :
-        return(("Active",Ibase,Icollector,VEC))
+        TEMP = "KVL 1: -VBB + (RB)*IB + VEB(ACTIVE) = 0 \n KVL 2: -VCC + RC * IC + VEC = 0 \n IC = BETA * IB"
+        return((("Active",Ibase,Icollector,VEC),draw_circuit1_PNP_Active,TEMP))
     else:
         Ibase = (VBB-0.8)/RB
         Icollector = (VCC - 0.2) / RC
         VEC = 0.2
-        return(("Sat",Ibase,Icollector,VEC))
+        TEMP = "KVL 1: -VBB + (RB)*IB + VEB(sat) = 0 \n IE = IC + IB"
+        return((("Sat",Ibase,Icollector,VEC),draw_circuit1_PNP_Sat,TEMP))
 
 
--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from PIL import Image, ImageTk
 import io
 from tkinter import font, ttk
 import Circuit1 as C
-
+from tkinter import simpledialog
 # تابع برای نمایش رسم در Tkinter
 def show_drawing_in_tkinter(drawing, frame_to_display):
     # ذخیره رسم در حافظه به عنوان تصویر PNG
@@ -55,10 +55,9 @@ frame_top.grid_rowconfigure(1, weight=1)
 frame = tk.Frame(root)
 frame.pack()
 
+Circuit_inputs= {"Circuit 1":[5,"VCC","VBB","RB","RC","BETA"]}
 
-# تابع‌های مختلف برای رسم مدارهای مختلف
-def draw_circuit(selected_circuit):
-    # پاک کردن محتوای هر فریم قبل از رسم مدار جدید
+def clean_frames():
     for widget in frame_1.winfo_children():
         widget.destroy()
     for widget in frame_2.winfo_children():
@@ -68,42 +67,140 @@ def draw_circuit(selected_circuit):
     for widget in frame_4.winfo_children():
         widget.destroy()
 
+def Analysis_circuit(inputs,selected_circuit,selected_Type):
+    clean_frames()
     d = schemdraw.Drawing()
-    
+    result = {}
     if selected_circuit == "Circuit 1":
-        d = C.draw_circuit1_NPN(d)
+        if(selected_Type == "NPN"):
+            d = C.draw_circuit1_NPN(d,inputs["RB"][1],inputs["VCC"][1],inputs["RC"][1],inputs["VBB"][1])
+            show_drawing_in_tkinter(d, frame_1)
+            result = C.Analysis_for_circuit1_NPN(inputs["VBB"][0],inputs["RB"][0],inputs["BETA"][0],inputs["VCC"][0],inputs["RC"][0])
+            d = schemdraw.Drawing()
+            d = result[1](d,inputs["RB"][1],inputs["VCC"][1],inputs["RC"][1],inputs["VBB"][1])
+        else:
+            d = C.draw_circuit1_PNP(d,inputs["RB"][1],inputs["VCC"][1],inputs["RC"][1],inputs["VBB"][1])
+            show_drawing_in_tkinter(d, frame_1)
+            result = C.Analysis_for_circuit1_PNP(inputs["VBB"][0],inputs["RB"][0],inputs["BETA"][0],inputs["VCC"][0],inputs["RC"][0])
+            d = schemdraw.Drawing()
+            d = result[1](d,inputs["RB"][1],inputs["VCC"][1],inputs["RC"][1],inputs["VBB"][1])
+        
+    elif selected_circuit == "Circuit 2":
+        pass
+        # d = C.draw_circuit1_PNP(d)
+        # show_drawing_in_tkinter(d, frame_1)
+    elif selected_circuit == "Circuit 3":
+        pass  # در صورت نیاز به اضافه کردن مدارهای جدید
+    elif selected_circuit == "Circuit 18":
+        pass  # در صورت نیاز به اضافه کردن مدارهای جدید
+
+    
+    show_drawing_in_tkinter(d, frame_2)
+    text=""
+    text+=f"State of BJT : {result[0][0]}\n"
+    text+=f"IB : {result[0][1]} mA\n"
+    text+=f"IC : {result[0][2]} mA\n"
+    text+=f"VCE (NPN) or VEC (PNP) : {result[0][3]} V\n"
+    label = tk.Label(frame_4, text=text, font=("Helvetica", 10))
+    label.pack(padx=10, pady=10)
+    label1 = tk.Label(frame_3, text=result[2], font=("Helvetica", 10))
+    label1.pack(padx=10, pady=10)
+    
+    
+    
+   
+# تابع‌های مختلف برای رسم مدارهای مختلف
+def draw_circuit(selected_circuit,selected_Type):
+    clean_frames()
+    d = schemdraw.Drawing()
+    label = tk.Label(frame_1, text= selected_circuit, font=("Helvetica", 11,'bold'))
+    label.pack(padx=10, pady=10)
+    if selected_circuit == "Circuit 1":
+        if(selected_Type == "NPN"):
+            d = C.draw_circuit1_NPN(d)
+        else:
+            d = C.draw_circuit1_PNP(d)
         show_drawing_in_tkinter(d, frame_1)  # نمایش مدار در قسمت بالا چپ
     elif selected_circuit == "Circuit 2":
-        d = C.draw_circuit1_PNP(d)
-        show_drawing_in_tkinter(d, frame_1)
+        pass
+        # d = C.draw_circuit1_PNP(d)
+        # show_drawing_in_tkinter(d, frame_1)
     elif selected_circuit == "Circuit 3":
         pass  # در صورت نیاز به اضافه کردن مدارهای جدید
     elif selected_circuit == "Circuit 18":
         pass  # در صورت نیاز به اضافه کردن مدارهای جدید
     
-   
+def open_input_window(selected_circuit,selected_Type):
+    # ایجاد یک پنجره جدید
+    input_window = tk.Toplevel(root)
+    # از کاربر می‌خواهیم که تعداد ورودی‌ها را وارد کند
+    num_inputs = Circuit_inputs[selected_circuit][0]
+    # لیستی برای ذخیره ورودی‌ها
+    input_entries = []
+    
+    # ایجاد ورودی‌ها به تعداد مورد نظر
+    for i in range(num_inputs):
+        label = tk.Label(input_window, text= Circuit_inputs[selected_circuit][i+1])
+        label.grid(row=i, column=0, padx=10, pady=5)
+        entry = tk.Entry(input_window)
+        entry.grid(row=i, column=1, padx=10, pady=5)
+        input_entries.append(entry)
+        
+    
+    # دکمه برای ارسال ورودی‌ها
+    def submit_inputs():
+        inputs={}
+        for i in range(num_inputs):
+            temp=""
+            if Circuit_inputs[selected_circuit][i+1]=="VCC" or Circuit_inputs[selected_circuit][i+1]=="VBB":
+                temp=input_entries[i].get()+"V"
+            elif Circuit_inputs[selected_circuit][i+1]=="RB" or Circuit_inputs[selected_circuit][i+1]=="RC" or Circuit_inputs[selected_circuit][i+1]=="RE" :
+                temp=input_entries[i].get()+"K ohms"
+            else :
+                temp=input_entries[i].get()
+            inputs[Circuit_inputs[selected_circuit][i+1]] = (int(input_entries[i].get()),temp)
+        print(inputs)
+        Analysis_circuit(inputs,selected_circuit,selected_Type)
+        
+        input_window.destroy()
+
+    submit_button = tk.Button(input_window, text="OK", command=submit_inputs)
+    submit_button.grid(row=num_inputs, columnspan=2, pady=10)
+
 
 # تنظیمات فونت دکمه‌ها
 button_font = font.Font(family="Helvetica", size=12, weight="bold")
 
 # ایجاد منوی کشویی برای انتخاب مدار
-options = [f"Circuit {i}" for i in range(1, 19)]  # ایجاد لیست مدارها از Circuit 1 تا Circuit 18
+options = [f"Circuit {i}" for i in range(1, 10)]  # ایجاد لیست مدارها از Circuit 1 تا Circuit 18
 selected_option = tk.StringVar(root)
 selected_option.set(options[0])  # پیش‌فرض اولین گزینه
+
+Types = ["NPN","PNP"]
+selected_option1 = tk.StringVar(root)
+selected_option1.set(Types[0])  # پیش‌فرض اولین گزینه
 
 # فریم پایین برای دکمه‌ها و منوی کشویی
 bottom_frame = tk.Frame(root, bg="lightgray")
 bottom_frame.pack(side=tk.BOTTOM, fill=tk.X, padx=10, pady=10)
 
 # منوی کشویی برای انتخاب مدار
+dropdown_menu = ttk.Combobox(bottom_frame, textvariable=selected_option1, values=Types, font=button_font)
+dropdown_menu.set("Select Type of BJT")  # تنظیم مقدار پیش‌فرض
+dropdown_menu.pack(side=tk.LEFT, padx=10, pady=10)
+
 dropdown_menu = ttk.Combobox(bottom_frame, textvariable=selected_option, values=options, font=button_font)
 dropdown_menu.set("Select Circuit")  # تنظیم مقدار پیش‌فرض
 dropdown_menu.pack(side=tk.LEFT, padx=10, pady=10)
 
 # دکمه برای رسم مدار انتخاب شده
-draw_button = tk.Button(bottom_frame, text="Draw Selected Circuit", command=lambda: draw_circuit(selected_option.get()))
+draw_button = tk.Button(bottom_frame, text="Draw Selected Circuit", command=lambda: draw_circuit(selected_option.get(),selected_option1.get()))
 draw_button.config(font=button_font, bg="green", fg="white", padx=15, pady=10, relief="raised", bd=3)
 draw_button.pack(side=tk.RIGHT, padx=10, pady=10)
+
+draw_button = tk.Button(bottom_frame, text="Enter inputs", command=lambda: open_input_window(selected_option.get(),selected_option1.get()))
+draw_button.config(font=button_font, bg="red", fg="white", padx=15, pady=10, relief="raised", bd=3)
+draw_button.pack(side=tk.TOP, padx=10, pady=10)
 
 # اجرای پنجره اصلی
 root.mainloop()
