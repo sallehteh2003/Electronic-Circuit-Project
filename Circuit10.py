246728012a5dfdd0e6fed912f11362cd539c5fca
Mohmmad Saleh Tehrani <saleh.teh2003@gmail.com>
2025-02-04 22:08:30 +0330

    fix Circuit10.py

--- Circuit10.py
+++ Circuit10.py
@@ -103,11 +103,11 @@ def draw_circuit10_NPN(d,RB1="RB1",RB2="RB2",VCC="VCC",RC="RC",VEE="VEE",RE="RE"
     d += elm.Line().length(0.5).left()
     d += elm.Ground()
     d += elm.Line().length(0.5).right()
-    d += elm.BatteryCell().down().reverse().label(VEE)
+    d += elm.BatteryCell().down().label(VEE)
     d += elm.Line().length(2).right()
     d += elm.Resistor().up().label(RB2)
     d += elm.Line().length(0.5).right()
-    transistor = d.add( elm.BjtNpn(circle=True).label('Q1').right())
+    transistor = d.add( elm.BjtNpn(circle=False).label('Q1').right())
     d += elm.Line().length(0.5).left().at(transistor.base)
     d += elm.Resistor().up().label(RB1)
     d += elm.Line().length(2).left()
@@ -129,7 +129,7 @@ def draw_circuit10_NPN_ACTIVE(d,RB1="RB1",RB2="RB2",VCC="VCC",RC="RC",VEE="VEE",
     d += elm.Line().length(0.5).left()
     d += elm.Ground()
     d += elm.Line().length(0.5).right()
-    d += elm.BatteryCell().down().reverse().label(VEE)
+    d += elm.BatteryCell().down().label(VEE)
     d += elm.Line().length(2).right()
     d += elm.Resistor().up().label(RB2)
     Battery = d.add( elm.BatteryCell().right().label('VBE(ACTIVE)').scale(0.7))
@@ -151,7 +151,7 @@ def draw_circuit10_NPN_SAT(d,RB1="RB1",RB2="RB2",VCC="VCC",RC="RC",VEE="VEE",RE=
     d += elm.Line().length(0.5).left()
     d += elm.Ground()
     d += elm.Line().length(0.5).right()
-    d += elm.BatteryCell().down().reverse().label(VEE)
+    d += elm.BatteryCell().down().label(VEE)
     d += elm.Line().length(2).right()
     d += elm.Resistor().up().label(RB2)
     Battery = d.add( elm.BatteryCell().right().label('VBE(ACTIVE)').scale(0.7))
@@ -173,7 +173,7 @@ def draw_circuit10_NPN_OFF(d,RB1="RB1",RB2="RB2",VCC="VCC",RC="RC",VEE="VEE",RE=
     d += elm.Line().length(0.5).left()
     d += elm.Ground()
     d += elm.Line().length(0.5).right()
-    d += elm.BatteryCell().down().reverse().label(VEE)
+    d += elm.BatteryCell().down().label(VEE)
     d += elm.Line().length(2).right()
     d += elm.Resistor().up().label(RB2)
     d += elm.Line().length(0.5).right()
@@ -198,7 +198,7 @@ def draw_circuit10_NPN_OFF(d,RB1="RB1",RB2="RB2",VCC="VCC",RC="RC",VEE="VEE",RE=
 def Analysis_for_circuit10_NPN(RB2,RB1,VEE,VCC,RC,RE,BETA):
     VTH = (RB2/(RB1+RB2))*(VCC+VEE)- VEE
     RTH = (RB1*RB2)/(RB1+RB2)
-    IB = (VEE-VTH-0.7)/(RTH+((BETA+1)*RE))
+    IB = (VEE+VTH-0.7)/(RTH+((BETA+1)*RE))
     if IB <= 0 :
         return((("off",0,0,VCC+VEE),draw_circuit10_NPN_OFF,"KVL 1: -VBB + (RB)*IB + VBE(ACTIVE) = 0"))
     IC = BETA * IB
@@ -215,20 +215,20 @@ def Analysis_for_circuit10_NPN(RB2,RB1,VEE,VCC,RC,RE,BETA):
         return((("Sat",solution[IC],solution[IB],0.2),draw_circuit10_NPN_SAT,TEMP))
 
 def Analysis_for_circuit10_PNP(RB2,RB1,VEE,VCC,RC,RE,BETA):
-        VTH = (RB2/(RB1+RB2))*( (-1 * VCC) - VEE) + VEE
+        VTH = (RB2/(RB1+RB2))*( (-1 * (VCC + VEE) )) + VEE
         RTH = (RB1*RB2)/(RB1+RB2)
         IB = (VEE-VTH-0.7)/(RTH+((BETA+1)*RE))
         if IB <= 0 :
-            return((("off",0,0,VCC+VEE),draw_circuit10_NPN_OFF,"KVL 1: -VBB + (RB)*IB + VBE(ACTIVE) = 0"))
+            return((("off",0,0,VCC+VEE),draw_circuit10_PNP_off,"KVL 1: -VBB + (RB)*IB + VEB(ACTIVE) = 0"))
         IC = BETA * IB
         VEC = VCC + VEE -  (IC * ( RC + ((BETA+1) / BETA *RE)))
         if VEC > 0.2 :
-            TEMP = "KVL 1: -VTH + (RTH)*IB + VBE(ACTIVE) + RE * (BETA + 1) * IB = 0 \n KVL 2: -VCC + RC * IC + VCE + RE * (BETA + 1/ BETA) -VEE  = 0 \n IC = BETA * IB"
-            return((("Active",IB,IC,VEC),draw_circuit10_NPN_ACTIVE,TEMP))
+            TEMP = "KVL 1: -VTH + (RTH)*IB + VEB(ACTIVE) + RE * (BETA + 1) * IB = 0 \n KVL 2: -VCC + RC * IC + VEC + RE * (BETA + 1/ BETA) -VEE  = 0 \n IC = BETA * IB"
+            return((("Active",IB,IC,VEC),draw_circuit10_PNP_ACTIVE,TEMP))
         else:
             IC, IB = symbols('IC IB')
-            eq1 = Eq( -VTH + RTH * IB + 0.8 + (IC + IB) * RE -VEE , 0 )
+            eq1 = Eq( +VTH + RTH * IB + 0.8 + (IC + IB) * RE -VEE , 0 )
             eq2 = Eq( -VCC + IC * RC + 0.2 + RE * (IC + IB) , 0 )
             solution = solve((eq1, eq2), (IC ,IB))
-            TEMP = "KVL 1: -VTH + (RTH)*IB + VBE(sat) + RE * IE - VEE = 0 \n KVL 2: -VCC + IC *RC + VCE(SAT) + IE * RE   \n IE = IC + IB"
-            return((("Sat",solution[IC],solution[IB],0.2),draw_circuit10_NPN_SAT,TEMP))
+            TEMP = "KVL 1: +VTH + (RTH)*IB + VEB(sat) + RE * IE - VEE = 0 \n KVL 2: -VCC + IC *RC + VEC(SAT) + IE * RE   \n IE = IC + IB"
+            return((("Sat",solution[IC],solution[IB],0.2),draw_circuit10_PNP_SAT,TEMP))
